f64c182fc843adf6b1074e450b88b1c367f7dbf1
Jos Vermoesen <info@rv.be>
2023-09-29 21:42:04 +0200

    BE, DE, FR, GB, LU, NL ready for implementing

--- src/app/modules/csv2json/csv2json.component.ts
+++ src/app/modules/csv2json/csv2json.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import csv2json from 'csvjson-csv2json';
-// Use the csv2json library here
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-csv2json',
@@ -12,20 +12,55 @@ import csv2json from 'csvjson-csv2json';
 export class Csv2JsonComponent {
   csvContent = '';
   jsonContent = '';
+  jsonArray = [];
   busy = false;
+  rowsMessage = '';
+
+  // assetsUrl = 'WHO-COVID-19-BE-data';
+  // assetsUrl = 'WHO-COVID-19-DE-data';
+  // assetsUrl = 'WHO-COVID-19-FR-data';
+  // assetsUrl = 'WHO-COVID-19-GB-data';
+  // assetsUrl = 'WHO-COVID-19-NL-data';
+  assetsUrl = 'WHO-COVID-19-LU-data';
 
   constructor(private http: HttpClient, private ts: TranslateService) {}
 
-  loadCsvExampleFromAssets() {
+  loadCsvConvert2JsonAndSave() {
     this.busy = true;
     this.http
-      .get('assets/WHO-COVID-19-global-data.csv', {
+      .get('assets/' + this.assetsUrl + '.csv', {
         responseType: 'text',
       })
-      .subscribe((data) => {
-        const json = csv2json(data);
-        console.log(json);
-        this.busy = false;
+      .subscribe({
+        next: (data) => {
+          this.rowsMessage = ' converting ' + data.length + ' rows';
+          this.csvContent = data;
+          this.jsonArray = csv2json(this.csvContent);
+        },
+        error: (error) => {
+          console.error('There was an error!', error);
+        },
+        complete: () => {
+          console.log('completed');
+          this.jsonContent = JSON.stringify(this.jsonArray);
+          this.saveJsonFile();
+          // console.log(this.jsonContent);
+        },
       });
+  }
+
+  async saveJsonFile() {
+    const finished = (error: any) => {
+      if (error) {
+        console.log('failed');
+        console.log(error);
+      } else {
+        console.log('finished');
+      }
+    };
+
+    const blob = new Blob([this.jsonContent], { type: 'application/json' });
+    saveAs(blob, this.assetsUrl + '.json');
+    this.busy = false;
   }
 }
